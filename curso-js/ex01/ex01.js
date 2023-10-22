36d8c23afd22bcf02f19df72c19fc1dcb374409c
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-22 12:09:44 -0300

    Exercício de variáveis 1

---
+++ curso-js/ex01/ex01.js
@@ -0,0 +1,15 @@
+// 1. Crie uma variável para armazenar o nome do carro e ponha o valor Ferrari. E exiba-a.
+
+let carro = "Ferrari";
+console.log(carro);
+
+//2. Crie uma variável para armazenar o preço do bolo, que custa R$ 19,99. E exiba=a.
+
+let precoBolo = 19.99;
+console.log(precoBolo);
+
+//3. Corrija o código a seguir (e exiba-o). let "cidade" = São Paulo
+
+let cidade = "São Paulo";
+console.log(cidade);
+
