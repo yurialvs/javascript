295c9081306751bbb5312f79b7d213b4e88dff61
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-22 15:20:34 -0300

    Condicionais IF - ELSE

---
+++ curso-js/condicionais/script.js
@@ -0,0 +1,18 @@
+let idade = 30;
+
+if (idade > 17) {
+    console.log("Você é MAIOR de idade.");
+} else {
+    console.log("Você é MENOR de idade.");
+}
+
+/*
+
+< MENOR QUE
+> MAIOR QUE
+<= MENOR OU IGUAL
+>= MAIOR OU IGUAL
+== IGUAL
+!= DIFERENTE
+
+*/
