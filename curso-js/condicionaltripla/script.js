393b5aed78183272396a871b5d6f08862fc8b83c
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-24 09:11:08 -0300

    Condicional Tripla

---
+++ curso-js/condicionaltripla/script.js
@@ -0,0 +1,10 @@
+let idade = 27;
+
+if (idade < 18) {
+    console.log("Você é uma criança.")
+} else if (idade >= 18 && idade < 60) {
+    console.log("Você é um adulto.")
+} else if (idade >= 60) {
+    console.log("Você é um idoso.")
+}
+
