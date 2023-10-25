4f6ba097bd14b08648bb9f14bffc9296bf899b01
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-25 08:18:15 -0300

    Switch

---
+++ curso-js/switch/script.js
@@ -0,0 +1,19 @@
+let profession = "policial";
+
+switch(profession) {
+    case "fiscal":
+        console.log("Sua camisa será VERDE.");
+        break;
+
+    case "bombeiro":
+        console.log("Sua camisa será VERMELHA.");
+        break;
+    
+    case "policial":
+        console.log("Sua camisa será PRETA.");
+        break;
+
+    default:
+        console.log("Sua camisa será BRANCA.");
+        break;
+}
