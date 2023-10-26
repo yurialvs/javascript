942cd5a353776a523cc5f4acf2213fd787df1a00
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-26 08:36:31 -0300

    Parâmetros em function

---
+++ curso-js/funcao/script2.js
@@ -0,0 +1,12 @@
+function somar(n1, n2) {
+    let resultado = n1 + n2;
+    console.log("Resultado: " + resultado);
+}
+
+somar(12, 15);
+
+function nomeCompleto(nome, sobrenome) {
+    console.log(`Nome completo: ${nome} ${sobrenome}`);
+}
+
+nomeCompleto("Yuri", "Alves");
