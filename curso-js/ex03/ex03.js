cad3ad3b95b6161270a8532c7bf50fec0e52cbea
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-27 07:51:17 -0300

    exercício 3

---
+++ curso-js/ex03/ex03.js
@@ -0,0 +1,12 @@
+/*
+Calcule a porcentagem entre 2 números.
+Exemplo: 25% de 40 é 10
+Fórmula da porcentagem: (y / x) * 100
+
+Uso da função:
+let x= 40;
+let y = 10;
+let pct = calcPct(x, y);
+console.log(`${pct}% de ${x} é ${y}`);
+*/
+
