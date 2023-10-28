128d219e82f550bcda5d26b0b0bcc11d61259c6b
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-28 13:47:59 -0300

    Arrow Function

---
+++ curso-js/arrow-function/script.js
@@ -0,0 +1,16 @@
+/*
+function somar(x, y){
+    return x + y;
+}
+*/
+
+const somar = (x, y) => x + y;
+
+console.log(somar(10,5));
+
+const somar2 = (a, b) => {
+    let resultado = a + b;
+    return resultado;
+}
+
+console.log(somar2(28,4));
