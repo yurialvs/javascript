3c8b6a8e24491781f4eef6c323a8b1c6587ad20e
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-28 15:25:32 -0300

    array

---
+++ curso-js/array/script.js
@@ -0,0 +1,20 @@
+let colors = ["blue", "red", "green"];
+
+/*
+0: blue
+1: red
+2: green
+*/
+
+console.log(colors[1]);
+
+let lista = ["carro", ["moto", "bicicleta"]];
+
+/*
+0: carro
+1: [moto, bicicleta]
+    0: moto
+    1: bicicleta
+*/
+
+console.log(lista[1][0]);
