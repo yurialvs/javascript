41ca462c9c7c46dc5f8b97372855e99bdfcbb0ac
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-10-31 22:55:29 -0300

    Operações de Array

---
+++ curso-js/op-array/script.js
@@ -0,0 +1,17 @@
+let bolo = ["água", "farinha", "ovo", "açucar", "chocolate"];
+
+// Adiciona item na lista
+// bolo[5] = "fermento"; 
+bolo.push("fermento"); 
+
+// Remove o último item da lista
+bolo.pop();
+
+// Remove o primeiro item da lista
+bolo.shift();
+
+// Quantidade de itens
+console.log(bolo.length); 
+
+// Lista
+console.log(bolo); 
