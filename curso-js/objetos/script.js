a88a6b8f0395c58f37bcb0e6217763cdbffdb1f7
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-01 09:42:19 -0300

    Alterando e acessando propriedades dos objetos

--- curso-js/objetos/script.js
+++ curso-js/objetos/script.js
@@ -17,3 +17,23 @@ console.log(`Força: ${personagem.habilidades.forca}.`);
 
 // Array dentro do objeto
 console.log(`Usa roupa ${personagem.roupa[0]}.`);
+
+// Alterando propriedades dos objetos
+personagem.pais = "Canadá";
+console.log(personagem.pais);
+
+personagem.habilidades.forca += 5;
+console.log(`Força: ${personagem.habilidades.forca}`);
+
+personagem.roupa.push("verde");
+console.log(personagem.roupa);
+
+// Acessando obejtos
+let veiculos = {
+    carros: [
+        {modelo: "Fiat", cor: "azul"},
+        {modelo: "Ferrari", cor: "vermelho"}
+    ]
+}
+
+console.log(veiculos.carros[1].modelo);
