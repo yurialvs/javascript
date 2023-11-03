6b46a66c441a05d99b59080c93c634c704dcc401
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-03 08:36:28 -0300

    Loop while

---
+++ curso-js/loop-while/node_c9d93e2663661.tmp.js
@@ -0,0 +1,7 @@
+// while === enquanto
+
+let numero = 0;
+
+while (numero < 10) {
+    console.log(`O número da vez é ${numero}`);
+}
---
+++ curso-js/loop-while/script.js
@@ -0,0 +1,13 @@
+// while === enquanto
+
+let numero = 0;
+
+while (numero === 0) {
+    console.log("TESTE");
+    break;
+}
+
+while (numero < 10) {
+    console.log(`O número da vez é ${numero}`);
+    numero++;
+}
