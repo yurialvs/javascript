20a0f349c99bd687f1bb2a89ce5dd37d07a5e3ab
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-03 09:13:11 -0300

    Exercício de loop 7

---
+++ curso-js/ex07/ex07.js
@@ -0,0 +1,16 @@
+// 1. Faça um loop que mostra todas a frutas
+
+let fruits = ["Uva", "Banana", "Manga"];
+
+for (let f in fruits) {
+    console.log(fruits[f]);
+}
+
+// 2. Conte de 1 até 100 através de um WHILE
+
+let cont = 1;
+
+while (cont <= 100) {
+    console.log(cont);
+    cont++;
+}
