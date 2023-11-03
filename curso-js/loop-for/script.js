129eed00eeda3d276307385a67eefbb05a36a59f
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-02 21:15:32 -0300

    Loop for

---
+++ curso-js/loop-for/script.js
@@ -0,0 +1,22 @@
+// for(variavel; condicional; incremento) {}
+
+for (let n = 1; n <= 3; n++) {
+    console.log("Frase teste " + n);
+}
+
+// Loop for em arrays
+
+let cores = ["preto", "branco", "azul", "vermelho"];
+
+for (let c = 0; c < cores.length; c++) {
+    console.log(cores[c]);
+}
+
+// Outro exemplo:
+
+let bolos = ["chocolate", "coco", "milho"];
+
+for (let b in bolos) {
+    console.log(bolos[b]);
+}
+
