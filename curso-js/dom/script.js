8e21b2dfc4cc7461e274d9e3cc24e8898c99aa9c
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-09 09:03:36 -0300

    DOM

---
+++ curso-js/dom/script.js
@@ -0,0 +1,23 @@
+/*
+DOM = Document Object Model
+
+document = Página do site
+
+window = Janela
+*/
+
+// Seleciona uma tag
+document.getElementsByTagName("h1");
+
+// Seleciona um id
+document.getElementById("teste");
+
+// Seleciona uma class
+document.getElementsByClassName("botao");
+
+// Seleciona uma tag, id, class. Seleciona semelhante ao CSS
+document.querySelector("#teste");
+
+// Seleciona tags especificas
+document.querySelectorAll("#teste ul li");
+
