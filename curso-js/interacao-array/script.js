34470de8f328cafd75704b0910d8ab327d28e557
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-08 23:22:27 -0300

    Interação de Array

---
+++ curso-js/interacao-array/script.js
@@ -0,0 +1,29 @@
+let fruits = ["Banana", "Uva", "Laranja", "Limão"];
+
+// Filtra as arrays
+let bigFruits = fruits.filter((item) => {
+    return item.length > 4;
+});
+
+console.log(bigFruits);
+
+// Verifica se todas palavras tem mais de 3 letras
+let ok = fruits.every((value) => {
+    return value.length > 3;
+});
+
+console.log(ok);
+
+// Verifica se algumas palavras tem mais de 3 letras
+let teste = fruits.some((value) => {
+    return value.length > 3;
+});
+
+console.log(teste);
+
+// Verifica se há uva
+if(fruits.includes("Uva")) {
+    console.log("Tem uva.");
+} else {
+    console.log("Não tem uva.");
+}
