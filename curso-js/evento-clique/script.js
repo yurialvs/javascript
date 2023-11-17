4ea91361db2a3420351756d86cca60b7f6b756e8
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-16 22:50:21 -0300

    Manipulando Elementos

--- curso-js/evento-clique/script.js
+++ curso-js/evento-clique/script.js
@@ -1,6 +1,17 @@
 function clicou(){
     const teste = document.querySelector("#teste");
     const p = teste.querySelector("p");
-    p.innerHTML = "<p>Texto 456</p>";
 // innerHTML: altera um elemento interno do HTML
+    p.innerHTML = "<p>Texto 456</p>";
+
+// children[0].append: adiciona algo após o elemento selecionado
+// Forma alternativa: p.children[0].innerHTML += " (alterado)";
+    p.children[0].append(" (alterado)");
+
+// adiciona novo elemento
+// Forma alternativa: p.innerHTML += "<p>Texto 789</p>";
+    let newText = document.createElement("p");
+    newText.innerText = "Texto 789";
+
+    p.appendChild(newText);
 }
