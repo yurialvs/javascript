2cf4f9a6ebc696e65b91c3fe8206e9055a27f58e
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2023-11-16 23:21:18 -0300

    Manipulando Elementos

---
+++ curso-js/evento-clique2/script.js
@@ -0,0 +1,9 @@
+function clicou(){
+    const teste = document.querySelector("#teste");
+    const p = teste.querySelector("p");
+
+// AFTER = Depois
+// BEFORE = Antes
+    p.after("Texto qualquer");
+
+}
