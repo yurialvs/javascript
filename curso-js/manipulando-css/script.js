5f6be718a8e3d8fd3f23d45503440f5c3303efb8
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2024-01-21 10:29:02 -0300

    Manipulando CSS

---
+++ curso-js/manipulando-css/script.js
@@ -0,0 +1,7 @@
+function clicou(){
+    const item = document.querySelector("p");
+
+    item.style.backgroundColor = "#ff0000";
+    item.style.fontSize = "20px";
+    item.innerText = "Item vermelho";
+}
