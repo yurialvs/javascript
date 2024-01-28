b6303f2105b89857cfaec821f0604170b24a905c
ʏ ᴜ ʀ ɪ <lops.yuri@gmail.com>
2024-01-28 11:22:26 -0300

    action

--- curso-js/orientacao-objetos/script.js
+++ curso-js/orientacao-objetos/script.js
@@ -1,6 +1,22 @@
 class Person{
+    
+    // Variáveis comuns
+    money = 10;
+    steps = 0;
+
+    // Construtor
     constructor(name, age){
         this.name = name;
         this.age = age;
     }
+
+    // Actions
+    takeAStep()
 }
+
+// Instância
+let p1 = new Person("João", 20);
+let p2 = new Person("Maria", 30);
+let p3 = new Person("Pedro", 40);
+
+console.log(p2.money);
